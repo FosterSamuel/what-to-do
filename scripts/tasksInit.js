6df570c35228ccf792325354f2f4f8bcd8b67c3e
Samuel Foster <samuelfosterweb@gmail.com>
2016-03-24 17:07:44 -0700

    Updated task list.

--- scripts/tasksInit.js
+++ scripts/tasksInit.js
@@ -10,9 +10,15 @@ var SORT_PRIORITY = function () {
 
 // createTask(taskList, name, time, priority);
 
-createTask(tasks, "Contribute on github", 5, 1);
-createTask(tasks, "Work on scholarship video", 2, 1);
-createTask(tasks, "Write paper", 4, 2);
+createTask(tasks, "Code left-pad", 0.01, 2);
 createTask(tasks, "Make bed", 0.1, 2);
-createTask(tasks, "Organize your coding assets", 1, 1);
+createTask(tasks, "Sketch website idea", .5, 0);
+createTask(tasks, "Contribute on github", 1, 0);
+createTask(tasks, "Work on scholarships", 2, 2);
+createTask(tasks, "Organize your coding assets", 3, 1);
+createTask(tasks, "Write English paper", 4, 2);
+createTask(tasks, "Build a JavaScript framework", 5, 2);
+createTask(tasks, "Call your parents", 6, 2);
+createTask(tasks, "Clean entire house", 7, 2);
+createTask(tasks, "Make good use of your job's time", 8, 2);
 createTask(tasks, "Learn all of JavaScript.", 200, 3);
